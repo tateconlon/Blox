fe1abe3db20ae3da9be5a45a6c6538ebbb9634ec
TastyTaters <tate.conlon@gmail.com>
2014-03-25 23:00:09 -0400

    Now an executable with level changing

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Alpha0))
+		{
+			Application.LoadLevel(0);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			Application.LoadLevel(1);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			Application.LoadLevel(2);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			Application.LoadLevel(3);
+		}
+	}
+}
